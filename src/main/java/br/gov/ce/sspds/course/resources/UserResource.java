6a00187449d0578240c50fa21db56613e39f19a8
Tatiana Brito <brito.tatiana@gmail.com>
2020-08-14 09:20:18 -0300

    aula 01

---
+++ src/main/java/br/gov/ce/sspds/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package br.gov.ce.sspds.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.ce.sspds.course.entities.User;
+
+@RestController
+@RequestMapping(value= "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L,"murilo","murilo@gmail.com","999","12345");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
