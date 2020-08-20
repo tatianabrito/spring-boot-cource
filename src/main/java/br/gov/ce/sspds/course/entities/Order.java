d16a2ea9cb65fd2a073ca158e647bbccfd553422
Tatiana Brito <brito.tatiana@gmail.com>
2020-08-20 19:14:30 -0300

    OrderStatus enum

--- src/main/java/br/gov/ce/sspds/course/entities/Order.java
+++ src/main/java/br/gov/ce/sspds/course/entities/Order.java
@@ -13,6 +13,8 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import br.gov.ce.sspds.course.entities.enums.OrderStatus;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -30,13 +32,16 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	private Integer orderStatus;
+	
 	public Order() {
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -52,12 +57,21 @@ public class Order implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 	public User getClient() {
 		return client;
 	}
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
 
 	@Override
 	public int hashCode() {
